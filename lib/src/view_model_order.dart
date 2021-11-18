8b88939adee407c43dc832276692ad41450cbd7b
Ihsan Isik <git@ihsan.dev>
2021-11-18 14:03:51 +0100

    Add a const constructor for ViewModelOrder

--- lib/src/view_model_order.dart
+++ lib/src/view_model_order.dart
@@ -3,4 +3,6 @@ import 'view_model_builder.dart';
 
 /// An order to the View from the ViewModel. (e.g. ShowSnackBar or PopRoute).
 /// See [ViewModelWidget] and [ViewModelBuilder].
-abstract class ViewModelOrder {}
+abstract class ViewModelOrder {
+  const ViewModelOrder();
+}
